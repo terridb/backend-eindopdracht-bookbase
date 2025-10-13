557b80808d321b9c45e838bd91317a852adcce58
terridb <terrideboer99@gmail.com>
2025-10-13 22:10:09 +0200

    Added authormapper tests for displayname and fixed grammar mistake userdto

--- src/main/java/com/terrideboer/bookbase/dtos/UserDto.java
+++ src/main/java/com/terrideboer/bookbase/dtos/UserDto.java
@@ -2,7 +2,7 @@ package com.terrideboer.bookbase.dtos;
 
 import com.terrideboer.bookbase.models.enums.Role;
 
-public class userDto {
+public class UserDto {
     public Long id;
     public String firstName;
     public String lastName;
--- src/test/java/com/terrideboer/bookbase/AuthorMapperTest.java
+++ src/test/java/com/terrideboer/bookbase/AuthorMapperTest.java
@@ -1,4 +1,49 @@
 package com.terrideboer.bookbase;
 
+import com.terrideboer.bookbase.dtos.AuthorDto;
+import com.terrideboer.bookbase.mappers.AuthorMapper;
+import com.terrideboer.bookbase.models.Author;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AuthorMapperTest {
+
+    @Test
+    public void toDtoDisplayNameShouldHandleSingleMiddleName() {
+        Author author = new Author();
+        author.setId(1L);
+        author.setFirstName("Sarah");
+        author.setMiddleNames("Janet");
+        author.setLastName("Maas");
+
+        AuthorDto dto = AuthorMapper.toDto(author);
+
+        assertEquals("Sarah J. Maas", dto.displayName);
+    }
+
+    @Test
+    public void toDtoDisplayNameShouldHandleTwoMiddleNames() {
+        Author author = new Author();
+        author.setId(1L);
+        author.setFirstName("Neil");
+        author.setMiddleNames("Richard MacKinnon");
+        author.setLastName("Gaiman");
+
+        AuthorDto dto = AuthorMapper.toDto(author);
+
+        assertEquals("Neil R.M. Gaiman", dto.displayName);
+    }
+
+    @Test
+    public void toDtoShouldHandleNoMiddleNames() {
+        Author author = new Author();
+        author.setId(1L);
+        author.setFirstName("Ali");
+        author.setLastName("Hazelwood");
+
+        AuthorDto dto = AuthorMapper.toDto(author);
+
+        assertEquals("Ali Hazelwood", dto.displayName);
+    }
 }
