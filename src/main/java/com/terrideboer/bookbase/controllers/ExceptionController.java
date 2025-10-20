6faa0d8233756c4ec2d4039c576491299583cbe2
terridb <terrideboer99@gmail.com>
2025-10-20 13:45:27 +0200

    Added exceptioncontroller

---
+++ src/main/java/com/terrideboer/bookbase/controllers/ExceptionController.java
@@ -0,0 +1,16 @@
+package com.terrideboer.bookbase.controllers;
+
+import com.terrideboer.bookbase.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
