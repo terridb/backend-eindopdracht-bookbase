2e0a685afb310cb52d1985bfc0acfef2c33e3812
terridb <terrideboer99@gmail.com>
2025-11-30 16:53:35 +0100

    Cleaned up code

--- src/main/java/com/terrideboer/bookbase/exceptions/PdfGenerationException.java
+++ src/main/java/com/terrideboer/bookbase/exceptions/PdfGenerationException.java
@@ -1,10 +1,6 @@
 package com.terrideboer.bookbase.exceptions;
 
 public class PdfGenerationException extends RuntimeException {
-    public PdfGenerationException(String message) {
-        super(message);
-    }
-
     public PdfGenerationException(String message, Throwable cause) {
         super(message, cause);
     }
